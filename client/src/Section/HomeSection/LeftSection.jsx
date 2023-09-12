171234c07e97716d1d02b19d487645372a8cf273
Aryan <108aryanmaurya@gmail.com>
2023-09-12 05:44:24 +0530

    Changed blogcard according to medium ,removed leftsection

--- client/src/Section/HomeSection/LeftSection.jsx
+++ client/src/Section/HomeSection/LeftSection.jsx
@@ -1,3 +1,23 @@
+import { BlogCard } from "../../Components/Common";
+import { CardData } from "../../Components/constants";
+
 export default function LeftSection() {
-  return <section className="theme-color ">LeftSection</section>;
+  return (
+    <section className="p-3 rounded-md w-full">
+      <div className=" grid grid-cols-1 justify-center  gap-6">
+        {CardData.map((card) => (
+          <BlogCard
+            Key={card.label}
+            label={card.label}
+            author={card.author}
+            imgURL={card.blog_imgURL}
+            imgURL2={card.author_imgURL}
+            category={card.category}
+            date={card.date}
+            tags={card.tags}
+          ></BlogCard>
+        ))}
+      </div>
+    </section>
+  );
 }
