1cbd66a39a20237c6dd0a630431ad058a7129624
Ashwin Maurya <ashwinmaurya30@gmail.com>
2023-09-12 19:15:12 +0530

    Remoed middle sectrion

--- client/src/Section/index.js
+++ client/src/Section/index.js
@@ -1,8 +1,6 @@
 import LeftSection from "./HomeSection/LeftSection";
-import MiddleSection from "./HomeSection/MiddleSection";
 import RightSection from "./HomeSection/RightSection";
 export {
-    MiddleSection,
     RightSection,
     LeftSection
 }
