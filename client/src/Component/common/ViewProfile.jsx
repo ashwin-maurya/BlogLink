aa81716aaee5853b0a320be79912b86b784b36a8
Ashwin Maurya <ashwinmaurya30@gmail.com>
2023-09-23 07:25:46 +0530

    Minor changes

--- client/src/Component/common/ViewProfile.jsx
+++ client/src/Component/common/ViewProfile.jsx
@@ -3,10 +3,8 @@ import React from "react";
 export default function ViewProfile() {
   return (
     <>
-      <div className="flex gap-2 text-lg leading-normal font-medium px-4 items-center">
-        <button className="dark:text-darkTextMain hover:text-primaryMain dark:hover:text-secondary">
-          View Profile
-        </button>
+      <div className="flex gap-2 hover:bg-slate-100 rounded-md text-lg leading-normal font-medium px-4 items-center dark:text-darkTextMain dark:hover:bg-darkBgMain  hover:text-primaryMain  dark:hover:text-secondary">
+        <button className=" text-left">View Profile</button>
       </div>
     </>
   );
