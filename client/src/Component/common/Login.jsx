b6b7883f3d6ed1124f91df22a8ec0099e5bd4fc5
Aryan <108aryanmaurya@gmail.com>
2023-09-27 18:52:54 +0530

    Added GoogleSignin

--- client/src/Component/common/Login.jsx
+++ client/src/Component/common/Login.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import Authentication from "./Authentication";
+import { onLogout } from "../../api/AuthAPI";
+import Profile from "./Profile";
+
 export default function Login() {
   const [showModal, setModal] = useState(false);
+  const localData = JSON.parse(localStorage.getItem("GoogleAuthData"));
+
   const ModalStatus = () => {
     setModal((showModal) => !showModal);
   };
@@ -9,14 +14,20 @@ export default function Login() {
     <>
       {showModal && <Authentication ModalStatus={ModalStatus}></Authentication>}
 
-      <div
-        className="flex gap-2 text-lg leading-normal font-medium  px-4"
-        onClick={ModalStatus}
-      >
-        <button className="dark:text-darkTextMain hover:text-primaryMain dark:hover:text-secondary">
-          Sign In
-        </button>
-      </div>
+      {JSON.parse(localStorage.getItem("GoogleAuthData")) != null ? (
+        <div className="group ">
+          <Profile img={localData.photoURL} name={localData.useName}></Profile>
+        </div>
+      ) : (
+        <div
+          className="flex gap-2 text-lg leading-normal font-medium  px-4"
+          onClick={ModalStatus}
+        >
+          <button className="dark:text-darkTextMain hover:text-primaryMain dark:hover:text-secondary">
+            Sign In
+          </button>
+        </div>
+      )}
     </>
   );
 }
